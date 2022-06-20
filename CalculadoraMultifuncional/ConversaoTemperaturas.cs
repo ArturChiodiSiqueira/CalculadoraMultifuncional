1d610ecfd536d9e8a0bded8b9dcde92e81f38f0d
ArturChiodiSiqueira <arturchiodi@outlook.com>
2022-06-19 21:56:08 -0300

    add

---
+++ CalculadoraMultifuncional/ConversaoTemperaturas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CalculadoraMultifuncional
+{
+    internal class ConversaoTemperaturas
+    {
+        public void ConverterTemperatura()
+        {
+            Console.WriteLine("se a temperatura que deseja converter estiver em °C, °F ou K...");
+            Console.WriteLine("informe (1) para °C, (2) para °F ou (3) para K");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+
+                    break;
+            }
+        }
+    }
+}
